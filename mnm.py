2c8afce9ed99710577081225d311b6890c237d5e
Diogo Fernandes <diogo-fernan@users.noreply.github.com>
2016-02-27 16:07:00 +0100

    Update mnm.py

--- mnm.py
+++ mnm.py
@@ -31,23 +31,23 @@ def hexdump (b, width=16, sep='.', offset=0x00):
 
 class Partition ():
 	def __init__ (self, mbr):
-		mask					= 0b0000001111111111
+		mask			= 0b0000001111111111
 
-		self.offset 			= 0x00
-		self.mbr				= mbr
-		self.bBootInd 			= mbr[0x01be]
-		self.bType				= mbr[0x1c2]
+		self.offset 		= 0x00
+		self.mbr		= mbr
+		self.bBootInd 		= mbr[0x01be]
+		self.bType		= mbr[0x1c2]
 		# bootable partition and NTFS
 		while self.bBootInd is not 0x80 and self.bType is not 0x70:
-			self.offset			+= 0x10 # up to four partition entries
-			self.bBootInd		= mbr[0x01be+self.offset]
-			self.bType			= mbr[0x1c2+self.offset]
+			self.offset	+= 0x10 # up to four partition entries
+			self.bBootInd	= mbr[0x01be+self.offset]
+			self.bType	= mbr[0x1c2+self.offset]
 
-		self.bHead				= mbr[0x01bf+self.offset]
-		self.bSector			= mbr[0x01c0+self.offset] >> 2
-		self.bCylinder			= unpack ('<H', mbr[0x01c0+self.offset:0x01c2+self.offset])[0] & mask
-		self.bLastHead			= mbr[0x01c3+self.offset]
-		self.bLastSector		= mbr[0x01c4+self.offset] >> 2
+		self.bHead		= mbr[0x01bf+self.offset]
+		self.bSector		= mbr[0x01c0+self.offset] >> 2
+		self.bCylinder		= unpack ('<H', mbr[0x01c0+self.offset:0x01c2+self.offset])[0] & mask
+		self.bLastHead		= mbr[0x01c3+self.offset]
+		self.bLastSector	= mbr[0x01c4+self.offset] >> 2
 		self.dLastCylinderSector= unpack ('<H', mbr[0x01c4+self.offset:0x01c6+self.offset])[0] & mask
 		self.dwRelativeSector	= unpack ('<L', mbr[0x01c6+self.offset:0x01ca+self.offset])[0]
 		self.dwNumberSectors	= unpack ('<I', mbr[0x01ca+self.offset:0x01ce+self.offset])[0]
@@ -57,30 +57,30 @@ class Partition ():
 		return hexdump (self.mbr)
 class NTFS ():
 	def __init__ (self, offset, ntfs):
-		self.offset					= offset
-		self.ntfs					= ntfs
+		self.offset			= offset
+		self.ntfs			= ntfs
 		self.sJumpInstruction 		= unpack ('<BBB', ntfs[0x00:0x03])[0]
-		self.sOemID					= unpack ('<Q', ntfs[0x03:0x0b])[0]
+		self.sOemID			= unpack ('<Q', ntfs[0x03:0x0b])[0]
 
-		self.wBytesPerSec			= unpack ('<H', ntfs[0x0b:0x0d])[0]
-		self.bSecPerClust			= ntfs[0x0d]
-		self.wReservedSec			= unpack ('<H', ntfs[0x0e:0x10])[0]
-		self.wReserved				= unpack ('<BBB', ntfs[0x10:0x13])[0]
-		self.wUnused1				= unpack ('<H', ntfs[0x13:0x15])[0]
+		self.wBytesPerSec		= unpack ('<H', ntfs[0x0b:0x0d])[0]
+		self.bSecPerClust		= ntfs[0x0d]
+		self.wReservedSec		= unpack ('<H', ntfs[0x0e:0x10])[0]
+		self.wReserved			= unpack ('<BBB', ntfs[0x10:0x13])[0]
+		self.wUnused1			= unpack ('<H', ntfs[0x13:0x15])[0]
 		self.bMediaDescriptor		= ntfs[0x15]
-		self.wUnused2				= unpack ('<H', ntfs[0x16:0x18])[0]
-		self.wSecPerTrack			= unpack ('<H', ntfs[0x18:0x1a])[0]
-		self.wNumberOfHeads			= unpack ('<H', ntfs[0x1a:0x1c])[0]
-		self.dwHiddenSec			= unpack ('<I', ntfs[0x1c:0x20])[0]
-		self.dwUnused3				= unpack ('<I', ntfs[0x20:0x24])[0]
-		self.dwUnused4				= unpack ('<I', ntfs[0x24:0x28])[0]
-		self.llTotalSec				= unpack ('<Q', ntfs[0x28:0x30])[0]
+		self.wUnused2			= unpack ('<H', ntfs[0x16:0x18])[0]
+		self.wSecPerTrack		= unpack ('<H', ntfs[0x18:0x1a])[0]
+		self.wNumberOfHeads		= unpack ('<H', ntfs[0x1a:0x1c])[0]
+		self.dwHiddenSec		= unpack ('<I', ntfs[0x1c:0x20])[0]
+		self.dwUnused3			= unpack ('<I', ntfs[0x20:0x24])[0]
+		self.dwUnused4			= unpack ('<I', ntfs[0x24:0x28])[0]
+		self.llTotalSec			= unpack ('<Q', ntfs[0x28:0x30])[0]
 		self.llMFTLogicalClustNum	= unpack ('<Q', ntfs[0x30:0x38])[0]
 		self.llMFTMirrLogicalClustNum = unpack ('<Q', ntfs[0x38:0x40])[0]
 		self.iClustPerMFTRecord		= unpack ('<I', ntfs[0x40:0x44])[0]
 		self.iClustPerIndexRecord	= unpack ('<I', ntfs[0x44:0x48])[0]
 		self.llVolumeSerialNum		= unpack ('<Q', ntfs[0x48:0x50])[0]
-		self.dwChecksum				= unpack ('<I', ntfs[0x50:0x54])[0]
+		self.dwChecksum			= unpack ('<I', ntfs[0x50:0x54])[0]
 
 		self.mftOffset = (self.bSecPerClust * self.wBytesPerSec) \
 						* self.llMFTLogicalClustNum + self.offset
@@ -95,7 +95,7 @@ class MFT ():
 		self.mft	= mft
 
 		# MFT Entry Header
-		self.sFileSignature = unpack ('<I', mft[0x00:0x04])[0]
+		self.sFileSignature	= unpack ('<I', mft[0x00:0x04])[0]
 		self.wFixupOffset 	= unpack ('<H', mft[0x04:0x06])[0]
 		self.wFixupSize 	= unpack ('<H', mft[0x06:0x08])[0]
 		self.llLogSeqNumber	= unpack ('<Q', mft[0x08:0x10])[0]
